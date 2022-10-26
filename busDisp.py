654d3c40f9812d724bb13b5de1a9ee6b01fb141b
Leo Cosgrove <leoco341@gmail.com>
2022-10-26 17:09:13 -0400

    sorting fix

--- busDisp.py
+++ busDisp.py
@@ -3,6 +3,7 @@ import urllib.request
 import re
 import numpy as np
 
+show_all_lines = 'on'
 bus_line = ['71B','71D']
 stop_number = ['3140','8312']
 direction = 'INBOUND'
@@ -11,7 +12,7 @@ extracted_data, line_data, clean_data, line_data, time_data = [[],[],[],[],[]]
 # Scrape Port Authority website for departures and clean up into useable data
 for x in range(len(bus_line)):
     URL = 'https://truetime.portauthority.org/bustime/wireless/html/eta.jsp?route=Port+Authority+Bus%3A'+bus_line[x]+\
-    '&direction=Port+Authority+Bus%3A'+direction+'&id=Port+Authority+Bus%3A'+stop_number[x]+'&showAllBusses=on'
+    '&direction=Port+Authority+Bus%3A'+direction+'&id=Port+Authority+Bus%3A'+stop_number[x]+'&showAllBusses='+show_all_lines
 
     print(URL)
 
@@ -19,13 +20,17 @@ for x in range(len(bus_line)):
         html = response.read().decode('utf-8')
 
     extracted_data = re.findall('(?<=<strong class="larger">).*?(?=</strong>)',html)
-    clean_data = [x.replace('&nbsp;', '').replace('#','').replace('MIN','') for x in extracted_data]
+    clean_data = [x.replace('&nbsp;', '').replace('#','').replace('MIN','').replace('DUE','0') for x in extracted_data]
 
     line_data += clean_data[::2]
     time_data += clean_data[1::2]
 
+# Convert time strings into ints
+time_data_int = [eval(x) for x in time_data]
+
 # Sort both lists by arrival time
-time_data_s, line_data_s = map(list, zip(*sorted(zip(time_data,line_data), reverse=True)))
+time_data_s, line_data_s = map(list, zip(*sorted(zip(time_data_int,line_data), reverse=False)))
+
 print(line_data_s)
 print(time_data_s)
 
