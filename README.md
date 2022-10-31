2e83dd211ec829953ade4685bd021c2f41e2d1b6
Leo Cosgrove <leoco341@gmail.com>
2022-10-30 22:25:16 -0400

    0 minutes replaced with DUE

--- README.md
+++ README.md
@@ -2,4 +2,7 @@
 ECE1895 Final Project
 
 Ideas:
-toggle between showing arrival time and time until arrival
+toggle between showing arrival time and minutes until arrival, maybe weather and time
+Turn off at night automatically
+color code lines
+split config file
--- busDisp.py
+++ busDisp.py
@@ -1,36 +1,42 @@
-from time import time
 import urllib.request
 import re
-import numpy as np
 
-show_all_lines = 'off'
-bus_line = ['71B','71D']
-stop_number = ['3140','8312']
+show_all_lines = 'on'
 direction = 'INBOUND'
-extracted_data, line_data, clean_data, line_data, time_data = [[],[],[],[],[]]
 
-# Scrape Port Authority website for departures and clean up into useable data
-for x in range(len(bus_line)):
-    URL = 'https://truetime.portauthority.org/bustime/wireless/html/eta.jsp?route=Port+Authority+Bus%3A'+bus_line[x]+\
-    '&direction=Port+Authority+Bus%3A'+direction+'&id=Port+Authority+Bus%3A'+stop_number[x]+'&showAllBusses='+show_all_lines
+def getArrivals(bus_lines,stop_numbers):
+    extracted_data, line_data, clean_data, time_data = [[],[],[],[]]
 
-    with urllib.request.urlopen(URL) as response:
-        html = response.read().decode('utf-8')
+    # Scrape Port Authority website for departures and clean up into useable data
+    for x in range(len(bus_lines)):
+        URL = 'https://truetime.portauthority.org/bustime/wireless/html/eta.jsp?route=Port+Authority+Bus%3A'+bus_lines[x]+\
+        '&direction=Port+Authority+Bus%3A'+direction+'&id=Port+Authority+Bus%3A'+stop_numbers[x]+'&showAllBusses='+show_all_lines
 
-    extracted_data = re.findall('(?<=<strong class="larger">).*?(?=</strong>)',html)
-    clean_data = [x.replace('&nbsp;', '').replace('#','').replace('MIN','').replace('DUE','0') for x in extracted_data]
+        with urllib.request.urlopen(URL) as response:
+            html = response.read().decode('utf-8')
 
-    line_data += clean_data[::2]
-    time_data += clean_data[1::2]
+        extracted_data = re.findall('(?<=<strong class="larger">).*?(?=</strong>)',html)
+        clean_data = [x.replace('&nbsp;', '').replace('#','').replace('MIN','').replace('DUE','0') for x in extracted_data]
 
-# Convert time strings into ints for sorting
-time_data_int = [eval(x) for x in time_data]
+        line_data += clean_data[::2]
+        time_data += clean_data[1::2]
 
-# Sort both lists by arrival time
-try:
-    time_data_sorted, line_data_sorted = map(list, zip(*sorted(zip(time_data_int,line_data), reverse=False)))
-except:
-    time_data_sorted, line_data_sorted = [[],['None :(']]
+    # Convert time strings into ints for sorting
+    time_data_int = [eval(x) for x in time_data]
 
-print(line_data_sorted)
-print(time_data_sorted)
+    # Sort both lists by arrival time
+    # Handle case of no buses coming
+    try:
+        time_data_sorted, line_data_sorted = map(list, zip(*sorted(zip(time_data_int,line_data), reverse=False)))
+    except ValueError:
+        time_data_sorted, line_data_sorted = [[],['None :(']]
+
+    return line_data_sorted, time_data_sorted
+
+apt_lines, apt_times = getArrivals(['71B','71D'],['3140','8312'])
+
+# Replace 0 with DUE
+apt_times = [x if x != 0 else 'DUE' for x in apt_times]
+
+print(apt_lines)
+print(apt_times)
