c22e777263db82f7ae77a5536ce126e7241c68db
Leo Cosgrove <leoco341@gmail.com>
2022-10-31 23:27:17 -0400

    Added date and time methods and general cleanup

--- busDisp.py
+++ busDisp.py
@@ -1,49 +1,77 @@
+from msilib.schema import Directory
 import os
-import xml.etree.ElementTree as ET
 import requests
-import json
 
 key = os.getenv('BUSTIME_API_KEY')
 
 # Access data through the official Port Authority API
-# Gets all buses and their predicted arrivals at specified stops
-def getAllArrivalsAPI(stop_numbers):
+# Gets all buses and their predicted arrivals and directions at specified stops
+# Usage: routes, times, directions = getAllArrivals(['3140','8312'])
+def getAllArrivals(stop_numbers):
     # Convert list to csv string
     stop_numbers_string = ','.join(stop_numbers)
-    # Build URL for API call
-    ploads = {'key':key,'stpid':stop_numbers_string,'format':'json','rtpidatafeed':'Port Authority Bus'}
-    URL = 'https://truetime.portauthority.org/bustime/api/v3/getpredictions'
 
+    # Build URL for API call
+    URL = 'https://truetime.portauthority.org/bustime/api/v3/getpredictions'
+    ploads = {'key':key,
+            'stpid':stop_numbers_string,
+            'format':'json',
+            'rtpidatafeed':'Port Authority Bus'}
 
     # Get XML response and parse
     raw = requests.get(URL,ploads).json()['bustime-response']['prd']
-    print(requests.get(URL,ploads).url)
-    routes = [raw[x]['rt'] for x in range(len(raw))]
-    times = [raw[x]['prdctdn'] for x in range(len(raw))]
+    routes = [x['rt'] for x in raw]
+    times = [x['prdctdn'] for x in raw]
+    directions = [x['rtdir'] for x in raw]
 
-    return routes,times
+    return routes,times,directions
 
 
 # Access data through the official Port Authority API
-# Gets certain bus lines and their predicted arrivals at specified stops
-def getSpecificArrivalsAPI(bus_lines,stop_numbers):
+# Gets certain bus lines and their predicted arrivals and directions at specified stops
+# Usage: routes, times, directions = getSpecificArrivals(['71B','71D'],['3140','8312'])
+def getSpecificArrivals(bus_lines,stop_numbers):
     # Convert list to csv string
     stop_numbers_string = ','.join(stop_numbers)
     bus_lines_string = ','.join(bus_lines)
 
     # Build URL for API call
     URL = 'https://truetime.portauthority.org/bustime/api/v3/getpredictions'
-    ploads = {'key':key,'stpid':stop_numbers_string,'format':'json','rtpidatafeed':'Port Authority Bus','rt':bus_lines_string}
+    ploads = {'key':key,
+            'stpid':stop_numbers_string,
+            'format':'json',
+            'rtpidatafeed':'Port Authority Bus',
+            'rt':bus_lines_string}
 
     # Get XML response and parse
     raw = requests.get(URL,ploads).json()['bustime-response']['prd']
-    print(requests.get(URL,ploads).url)
-    routes = [raw[x]['rt'] for x in range(len(raw))]
-    times = [raw[x]['prdctdn'] for x in range(len(raw))]
+    routes = [x['rt'] for x in raw]
+    times = [x['prdctdn'] for x in raw]
+    directions = [x['rtdir'] for x in raw]
 
-    return routes,times
+    return routes,times,directions
 
-routes, times = getAllArrivalsAPI(['3140','8312'])
-print(routes, times)
-routes, times = getSpecificArrivalsAPI(['71B','71D'],['3140','8312'])
-print(routes, times)
+
+def getTime():
+    # Build URL for API call
+    URL = 'https://truetime.portauthority.org/bustime/api/v3/gettime'
+    ploads = {'key':key,
+            'format':'json'}
+        
+    # Get XML response and parse
+    raw = requests.get(URL,ploads).json()['bustime-response']['tm']
+
+    return raw.split()[1]
+
+def getDate():
+    # Build URL for API call
+    URL = 'https://truetime.portauthority.org/bustime/api/v3/gettime'
+    ploads = {'key':key,
+            'format':'json'}
+        
+    # Get XML response and parse
+    raw = requests.get(URL,ploads).json()['bustime-response']['tm']
+
+    return raw.split()[0]
+
+print(getDate())
