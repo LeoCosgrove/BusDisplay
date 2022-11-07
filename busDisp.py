90371e679cc8631799b8c81bbbada56e69538c4c
Leo Cosgrove <leoco341@gmail.com>
2022-11-07 00:06:15 -0500

    error handling for no arrivals

--- busDisp.py
+++ busDisp.py
@@ -1,11 +1,10 @@
-from msilib.schema import Directory
 import os
 import requests
 
 key = os.getenv('BUSTIME_API_KEY')
 
-# Access data through the official Port Authority API
 # Gets all buses and their predicted arrivals and directions at specified stops
+# ex. ['71D', '64'], ['DUE', '5'], ['INBOUND', 'INBOUND']
 # Usage: routes, times, directions = getAllArrivals(['3140','8312'])
 def getAllArrivals(stop_numbers):
     # Convert list to csv string
@@ -19,16 +18,20 @@ def getAllArrivals(stop_numbers):
             'rtpidatafeed':'Port Authority Bus'}
 
     # Get XML response and parse
-    raw = requests.get(URL,ploads).json()['bustime-response']['prd']
-    routes = [x['rt'] for x in raw]
-    times = [x['prdctdn'] for x in raw]
-    directions = [x['rtdir'] for x in raw]
+    try:
+        raw = requests.get(URL,ploads).json()['bustime-response']['prd']
+        routes = [x['rt'] for x in raw]
+        times = [x['prdctdn'] for x in raw]
+        directions = [x['rtdir'] for x in raw]
+    except:
+        routes = ['No Arrivals :(']
+        times = []
+        directions = []
 
     return routes,times,directions
 
-
-# Access data through the official Port Authority API
 # Gets certain bus lines and their predicted arrivals and directions at specified stops
+# ex. ['71D'], ['DUE'], ['INBOUND']
 # Usage: routes, times, directions = getSpecificArrivals(['71B','71D'],['3140','8312'])
 def getSpecificArrivals(bus_lines,stop_numbers):
     # Convert list to csv string
@@ -44,14 +47,20 @@ def getSpecificArrivals(bus_lines,stop_numbers):
             'rt':bus_lines_string}
 
     # Get XML response and parse
-    raw = requests.get(URL,ploads).json()['bustime-response']['prd']
-    routes = [x['rt'] for x in raw]
-    times = [x['prdctdn'] for x in raw]
-    directions = [x['rtdir'] for x in raw]
-
+    try:
+        raw = requests.get(URL,ploads).json()['bustime-response']['prd']
+        routes = [x['rt'] for x in raw]
+        times = [x['prdctdn'] for x in raw]
+        directions = [x['rtdir'] for x in raw]
+    except:
+        routes = ['No Arrivals :(']
+        times = []
+        directions = []
+    
     return routes,times,directions
 
-
+# Gets current server time in 24 hr format ex. 23:24:43
+# Usage: time = getTime()
 def getTime():
     # Build URL for API call
     URL = 'https://truetime.portauthority.org/bustime/api/v3/gettime'
@@ -63,6 +72,8 @@ def getTime():
 
     return raw.split()[1]
 
+# Gets current date ex. 20221031
+# Usage: date = getDate()
 def getDate():
     # Build URL for API call
     URL = 'https://truetime.portauthority.org/bustime/api/v3/gettime'
@@ -74,4 +85,5 @@ def getDate():
 
     return raw.split()[0]
 
-print(getDate())
+print(getSpecificArrivals(['71B','71D'],['3140','8312']))
+print(getAllArrivals(['3140','8312']))
--- code.py
+++ code.py
@@ -4,7 +4,6 @@ import busio
 from adafruit_esp32spi import adafruit_esp32spi
 from digitalio import DigitalInOut
 import adafruit_esp32spi.adafruit_esp32spi_socket as socket
-import xml.etree.ElementTree as ET
 
 # get wifi details from secrets files
 try:
@@ -32,26 +31,7 @@ requests.set_socket(socket, esp)
 
 key = secrets["BUSTIME_API_KEY"]
 
-# Access data through the official Port Authority API
-# Gets all buses and their predicted arrivals at specified stops
-def getAllArrivalsAPI(stop_numbers):
-    # Convert list to csv string
-    stop_numbers_string = ','.join(stop_numbers)
+# implement methods here
 
-    # Build URL for API call
-    URL = 'https://truetime.portauthority.org/bustime/api/v3/getpredictions?key='+key+'&rtpidatafeed=Port%20Authority%20Bus&stpid='+stop_numbers_string
-
-    # Get XML response and parse
-    XML = requests.get(URL)
-    root = ET.fromstring(XML)
-
-    # Extract necessary data from parsed XML
-    routes, times = [[],[]]
-    for bus in root.findall('prd'):
-        routes.append(bus.find('rt').text)
-        times.append(bus.find('prdctdn').text)
-    
-    return routes,times
-
-routes, times = getAllArrivalsAPI(['3140','8312'])
+routes, times = getAllArrivals(['3140','8312'])
 print(routes,times)
